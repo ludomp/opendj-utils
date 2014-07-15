797cdb2f52d66ade884b9e9d08951763b75923db
Ludovic Poitou <ludovic.poitou@gmail.com>
2014-07-15 09:36:37 +0200

    Update schema-convert.py

--- schema-convert.py
+++ schema-convert.py
@@ -5,29 +5,21 @@ schema-convert.py
 
 # CDDL HEADER START
 #
+#
 # The contents of this file are subject to the terms of the
 # Common Development and Distribution License, Version 1.0 only
 # (the "License").  You may not use this file except in compliance
 # with the License.
 #
 # You can obtain a copy of the license at
-# trunk/opends/resource/legal-notices/OpenDS.LICENSE
-# or https://OpenDS.dev.java.net/OpenDS.LICENSE.
+# http://forgerock.org/license/CDDLv1.0.html..
 # See the License for the specific language governing permissions
 # and limitations under the License.
 #
-# When distributing Covered Code, include this CDDL HEADER in each
-# file and include the License file at
-# trunk/opends/resource/legal-notices/OpenDS.LICENSE.  If applicable,
-# add the following below this CDDL HEADER, with the fields enclosed
-# by brackets "[]" replaced with your own identifying information:
-#      Portions Copyright [yyyy] [name of copyright owner]
-#
-# CDDL HEADER END
-#
-#
 #      Copyright 2009 Sun Microsystems, Inc.
 #      Portions Copyright 2010-2012 ForgeRock Inc.
+#
+# CDDL HEADER END
 
 Created by Ludovic Poitou on 2009-01-28.
 
